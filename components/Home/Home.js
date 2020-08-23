0953012740d8297a750067a92b8553177e006dc8
kaenpe <knapalke@gmail.com>
2020-08-23 21:54:20 +0200

    home animation almost done

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -10,35 +10,50 @@ const StyledWelcome = styled(motion.main)`
   align-items: center;
   justify-content: center;
   margin: 0 auto;
+  position: relative;
+  z-index: 101;
 `;
 const StyledLogo = styled(motion.div)`
   display: flex;
   justify-content: center;
   flex-direction: column;
 `;
-const closeWelcome = {
-  open: { width: 'auto' },
-  closed: { width: 0 },
-};
+const StyledHome = styled.main`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  top: 0;
+  left: 0;
+  z-index: '100';
+  padding: 0 3% 0 3%3%;
+`;
 const Home = () => {
   const [showWelcome, setShowWelcome] = useState(true);
   return (
-    <div>
+    <>
       <AnimatePresence>
         {showWelcome && (
           <StyledWelcome
             transition={{ duration: 1 }}
-            animate={showWelcome ? 'open' : 'closed'}
-            variants={closeWelcome}
-            unmountOnExit
+            initial={{ width: '100vw' }}
+            animate={{ width: '100vw' }}
+            exit={{ width: '0' }}
           >
             <StyledLogo
               transition={{ duration: 1 }}
-              animate={showWelcome ? 'open' : 'closed'}
-              variants={closeWelcome}
+              initial={{ width: 'auto' }}
+              animate={{ width: 'auto' }}
+              exit={{ width: '0' }}
             >
               <img src='logo.jpg' alt='logo' />
-              {showWelcome && (
+              <motion.div
+                transition={{ duration: 0.5 }}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                style={{ margin: '0 auto' }}
+              >
+                {' '}
                 <Button
                   variant='contained'
                   color='primary'
@@ -51,14 +66,27 @@ const Home = () => {
                 >
                   <strong>PORTFOLIO</strong>
                 </Button>
-              )}
+              </motion.div>
             </StyledLogo>
           </StyledWelcome>
         )}
       </AnimatePresence>
-
-      <Typography variant='h1'>Dog</Typography>
-    </div>
+      <StyledHome
+        style={{
+          position: 'fixed',
+          height: '100vh',
+          width: '100vw',
+          top: 0,
+          left: 0,
+          zIndex: '100',
+        }}
+      >
+        {' '}
+        <Typography variant='h1' style={{ position: 'fixed' }}>
+          Dog
+        </Typography>
+      </StyledHome>
+    </>
   );
 };
 
