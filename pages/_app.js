4b7ed27b85ce28618bc5ca4b64b9f4f430227208
kaenpe <knapalke@gmail.com>
2020-08-25 15:41:31 +0200

    ss

--- pages/_app.js
+++ pages/_app.js
@@ -23,12 +23,7 @@ class MyApp extends App {
 
     return (
       <>
-        <Head>
-          <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
-          />
-        </Head>
+        <Head></Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
