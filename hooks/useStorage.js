4f5429319d55a8b7cd63cb0835ecbb2e1dd497ac
kaenpe <knapalke@gmail.com>
2020-08-29 16:33:39 +0200

    adjusted storage hook for better performance

--- hooks/useStorage.js
+++ hooks/useStorage.js
@@ -6,19 +6,18 @@ import {
 } from '../firebase/config';
 const useStorage = (file, title, category) => {
   const [progress, setProgress] = useState(0);
-  const [error, setError] = useState(null);
   const [url, setUrl] = useState(null);
 
   useEffect(() => {
     const storage = projectStorage.ref(file.name);
     const collection = projectFirestore.collection(`photoshop`);
-    storageRef.put(file).on(
+    storage.put(file).on(
       'state_changed',
       (snap) => {
         let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
         setProgress(percentage);
       },
-      (err) => setError(err),
+
       async () => {
         const url = await storage.getDownloadURL();
         collection.add({
@@ -32,7 +31,7 @@ const useStorage = (file, title, category) => {
     );
   }, [file]);
 
-  return { progress, error, url };
+  return { progress, url };
 };
 
 export default useStorage;
